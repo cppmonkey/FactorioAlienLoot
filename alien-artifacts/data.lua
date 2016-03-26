96fd3599482f711cd462a2f5d6bd6506041e8afb
Paul Parsons <parsons151185@gmail.com>
2016-03-26 02:30:52 +0000

    Increased usability and readability by conversion to function.

--- alien-artifacts/data.lua
+++ alien-artifacts/data.lua
@@ -1,66 +1,27 @@
-if data.raw["unit"] ~= nil then
-	if data.raw["unit"]["small-spitter"] ~= nil then
-		if data.raw["unit"]["small-spitter"].loot == nil then
-			data.raw["unit"]["small-spitter"].loot = {}
+function AddLootToEntity(entityType, entityName, itemName, probability, countMin, countMax)
+	if data.raw[entityType] ~= nil then
+		if data.raw[entityType][entityName] ~= nil then
+			if data.raw[entityType][entityName].loot == nil then
+				data.raw[entityType][entityName].loot = {}
+			end
+			table.insert(data.raw[entityType][entityName].loot, {item = itemName, probability = probability, count_min = countMin, count_max = countMax})
 		end
-		table.insert(data.raw["unit"]["small-spitter"].loot, {item = "alien-artifact", probability = .1, count_min = 1, count_max = 1})
-	end
-	if data.raw["unit"]["small-biter"] ~= nil then
-		if data.raw["unit"]["small-biter"].loot == nil then
-			data.raw["unit"]["small-biter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["small-biter"].loot, {item = "alien-artifact", probability = .1, count_min = 1, count_max = 1})
-	end
-	if data.raw["unit"]["medium-spitter"] ~= nil then
-		if data.raw["unit"]["medium-spitter"].loot == nil then
-			data.raw["unit"]["medium-spitter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["medium-spitter"].loot, {item = "alien-artifact", probability = .3, count_min = 1, count_max = 1})
-	end
-	if data.raw["unit"]["medium-biter"] ~= nil then
-		if data.raw["unit"]["medium-biter"].loot == nil then
-			data.raw["unit"]["medium-biter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["medium-biter"].loot, {item = "alien-artifact", probability = .3, count_min = 1, count_max = 1})
-	end
-	if data.raw["unit"]["big-spitter"] ~= nil then
-		if data.raw["unit"]["big-spitter"].loot == nil then
-			data.raw["unit"]["big-spitter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["big-spitter"].loot, {item = "alien-artifact", probability = .6, count_min = 1, count_max = 1})
-	end
-	if data.raw["unit"]["big-biter"] ~= nil then
-		if data.raw["unit"]["big-biter"].loot == nil then
-			data.raw["unit"]["big-biter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["big-biter"].loot, {item = "alien-artifact", probability = .6, count_min = 1, count_max = 1})
-	end
-	if data.raw["unit"]["behemoth-spitter"] ~= nil then
-		if data.raw["unit"]["behemoth-spitter"].loot == nil then
-			data.raw["unit"]["behemoth-spitter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["behemoth-spitter"].loot, {item = "alien-artifact", probability = 1, count_min = 1, count_max = 2})
-	end
-	if data.raw["unit"]["behemoth-biter"] ~= nil then
-		if data.raw["unit"]["behemoth-biter"].loot == nil then
-			data.raw["unit"]["behemoth-biter"].loot = {}
-		end
-		table.insert(data.raw["unit"]["behemoth-biter"].loot, {item = "alien-artifact", probability = 1, count_min = 1, count_max = 2})
 	end
 end
 
-if data.raw["turret"] ~= nil then
-	if data.raw["turret"]["medium-worm-turret"] ~= nil then
-		if data.raw["turret"]["medium-worm-turret"].loot == nil then
-			data.raw["turret"]["medium-worm-turret"].loot = {}
-		end
-		table.insert(data.raw["turret"]["medium-worm-turret"].loot, {item = "alien-artifact", probability = .5, count_min = 1, count_max = 1})
-	end
-	if data.raw["turret"]["big-worm-turret"] ~= nil then
-		if data.raw["turret"]["big-worm-turret"].loot == nil then
-			data.raw["turret"]["big-worm-turret"].loot = {}
-		end
-		table.insert(data.raw["turret"]["big-worm-turret"].loot,
-			{item = "alien-artifact", probability = 1, count_min = 1, count_max = 2})
-	end
-end
+AddLootToEntity("unit","small-biter","alien-artifact",.1,1,1)
+AddLootToEntity("unit","small-biter","alien-artifact",.1,1,1)
+
+AddLootToEntity("unit","medium-spitter","alien-artifact",.4,1,1)
+AddLootToEntity("unit","medium-biter","alien-artifact",.4,1,1)
+
+AddLootToEntity("unit","big-spitter","alien-artifact",.6,1,2)
+AddLootToEntity("unit","big-biter","alien-artifact",.6,1,2)
+
+AddLootToEntity("unit","behemoth-spitter","alien-artifact",1,2,5)
+AddLootToEntity("unit","behemoth-biter","alien-artifact",1,2,5)
+
+
+AddLootToEntity("turret","little-worm-turret","alien-artifact",.3,1,1)
+AddLootToEntity("turret","medium-worm-turret","alien-artifact",.6,1,2)
+AddLootToEntity("turret","big-worm-turret","alien-artifact",1,2,5)
