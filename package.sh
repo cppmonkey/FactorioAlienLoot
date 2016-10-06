76eb984676f498a095feede16a3376e6d8acc902
CppMonkey <parsons151185@gmail.com>
2016-10-06 01:00:04 +0100

    corrected folder paths in package.sh

--- package.sh
+++ package.sh
@@ -4,13 +4,13 @@ MODNAME=alien-artifact-loot
 VERSION=4
 
 function packageVersion {
-	mkdir $MODNAME\_0.$1
-	cp ./alien-artifacts/*.* ./$MODNAME\_0.$1
-        echo '  "version": "0.'$1'.'$2'",' >> ./$MODNAME\_0.$1/info.json
-	echo '  "factorio_version": "0.'$1'"' >> ./$MODNAME\_0.$1/info.json
-	echo '}' >> ./$MODNAME\_0.$1/info.json
-	zip -r alien-artifact-loot_0.$1.$2 ./$MODNAME\_0.$1/
-	rm -r ./$MODNAME\_0.$1/
+	mkdir $MODNAME\_0.$1.$2
+	cp ./alien-artifacts/*.* ./$MODNAME\_0.$1.$2
+        echo '  "version": "0.'$1'.'$2'",' >> ./$MODNAME\_0.$1.$2/info.json
+	echo '  "factorio_version": "0.'$1'"' >> ./$MODNAME\_0.$1.$2/info.json
+	echo '}' >> ./$MODNAME\_0.$1.$2/info.json
+	zip -r $MODNAME\_0.$1.$2.zip ./$MODNAME\_0.$1.$2/
+	rm -r ./$MODNAME\_0.$1.$2/
 }
 
 packageVersion 13 $VERSION
